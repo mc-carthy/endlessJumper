d7ee782f38433c2204c1787c932ce72f180a478f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-12 16:15:34 +0100

    Create PlayerController and ButtonController to control jumping

---
+++ Assets/scripts/ButtonController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	public void OnPointerDown (PointerEventData data) {
+		if (PlayerController.instance != null) {
+			PlayerController.instance.SetPower (true);
+		}
+	}
+
+	public void OnPointerUp (PointerEventData data) {
+		if (PlayerController.instance != null) {
+			PlayerController.instance.SetPower (true);
+		}
+	}
+}
---
+++ Assets/scripts/player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public static PlayerController instance;
+
+	private Rigidbody2D rb;
+	private Animator anim;
+	private float forceX, forceY;
+	private float thresholdX = 7f;
+	private float thresholdY = 14f;
+	private bool setPower, didJump;
+
+	private void Awake () {
+		MakeInstance ();
+	}
+
+	private void Update () {
+
+	}
+
+	public void SetPower (bool setPower) {
+		this.setPower = setPower;
+
+		if (setPower) {
+
+		} else {
+
+		}
+	}
+
+	private void MakeInstance () {
+		if (instance == null) {
+			instance = this;
+		}
+	}
+}
